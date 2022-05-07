222fb06f2d3b64ca89a9463b4e3ce3609876328a
Muneeb Ur Rehman <66199828+muneeb-250@users.noreply.github.com>
2022-05-07 13:06:37 +0500

    Updated Navbar

--- Navbar.jsx
+++ Navbar.jsx
@@ -4,13 +4,32 @@ const Navbar = () => {
   return (
     <Container>
       <Wrapper>
-        <Logo>CG Blends</Logo>
+        <Left>
+          <Logo>CG Blends</Logo>
+          <Menu>
+            <MenuItem>Home</MenuItem>
+            <MenuItem>Features</MenuItem>
+            <MenuItem>Services</MenuItem>
+            <MenuItem>Pricing</MenuItem>
+            <MenuItem>Contact</MenuItem>
+          </Menu>
+        </Left>
         <PrimaryBtn>Join Today!</PrimaryBtn>
       </Wrapper>
     </Container>
   );
 };
 
+const Menu = styled.ul`
+display: flex;
+list-style:none;
+`;
+const MenuItem = styled.li`
+margin-right: 30px;
+font-size: 20px;
+font-weight: bold;
+color:gray;
+`;
 const Container = styled.div`
   height: 50px;
 `;
@@ -21,19 +40,25 @@ const Logo = styled.h1`
 `;
 const Wrapper = styled.div`
   padding: 10px 20px;
+  align-items:center;
   display: flex;
   justify-content: space-between;
 `;
 const Left = styled.div`
-
-`
+  width: 60%;
+  display: flex;
+  align-items: center;
+  justify-content:space-between;
+`;
 const PrimaryBtn = styled.button`
   outline: none;
-  border: 2px solid dodgerblue;
-  border-radius: 4px;
-  padding: 8px 30px;
-  background: white;
-  color: #000;
+  border:none;
+  border: 2px solid white;
+  border-radius: 10px;
+  padding: 10px 15px;
+  background: dodgerblue;
+  color: #fff;
+  font-weight: bold;
   cursor: pointer;
 `;
 
